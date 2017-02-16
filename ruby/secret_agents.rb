7e6595faa7db7da0703769c11d5a5b7296fec1cb
Matt Kim <kimm5294@gmail.com>
2017-02-15 21:20:07 -0500

    write pseudocode for encrypt

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,12 @@
+=begin
+-define a method called encrypt
+-input is a string 
+-create loop that loops through each index of string
+-in loop use .next function on each index and a bang operator to save value of new index
+-end loop once all indexes have been looped
+-print new string outside of loop
+=end
+
+def encrypt 
+  
+end
