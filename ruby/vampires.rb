afe5328010c05b43a5261ed37128bd4a2ab1b5f7
Matt Kim <kimm5294@gmail.com>
2017-02-16 13:10:38 -0500

    prompt for number of employees and add loop to repeat survey that number of times using a counter

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,26 +1,37 @@
-puts "What is your name?"
-name= gets.chomp
+puts "How many employees will be processed?"
+number_employees = gets.chomp
+number_employees.to_i!
+counter = 0
 
-puts "How old are you?"
-age= gets.chomp
+while counter < number_employees
 
-puts "What year were you born?"
-birthyear= gets.chomp
+  puts "What is your name?"
+  name= gets.chomp
 
-puts "Do you like garlic bread? (y/n)"
-gbread= gets.chomp
+  puts "How old are you?"
+  age= gets.chomp
 
-puts "Do you want to enroll in company health insurance? (y/n)"
-insurance= gets.chomp
+  puts "What year were you born?"
+  birthyear= gets.chomp
 
-if name == "Drake Cula" || name == "Tu Fang"
-  puts "Definitely a vampire" 
-elsif ((2017-birthyear.to_i) != age.to_i) && gbread =="n" && insurance =="n"
-  puts "Almost certainly a vampire"
-elsif ((2017-birthyear.to_i) != age.to_i) && (gbread == "n" || insurance =="n")
-  puts "Probably a vampire"
-elsif ((2017-birthyear.to_i) == age.to_i) && (gbread =="y" || insurance =="y")
-  puts "Probably not a vampire"
-else 
-  puts "Results inconclusive"
+  puts "Do you like garlic bread? (y/n)"
+  gbread= gets.chomp
+
+  puts "Do you want to enroll in company health insurance? (y/n)"
+  insurance= gets.chomp
+
+  if name == "Drake Cula" || name == "Tu Fang"
+    puts "Definitely a vampire" 
+  elsif ((2017-birthyear.to_i) != age.to_i) && gbread =="n" && insurance =="n"
+    puts "Almost certainly a vampire"
+  elsif ((2017-birthyear.to_i) != age.to_i) && (gbread == "n" || insurance =="n")
+    puts "Probably a vampire"
+  elsif ((2017-birthyear.to_i) == age.to_i) && (gbread =="y" || insurance =="y")
+    puts "Probably not a vampire"
+  else 
+    puts "Results inconclusive"
+  end
+
+  counter+=1
+
 end
