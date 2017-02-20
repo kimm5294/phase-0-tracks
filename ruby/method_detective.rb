024eee772bdd11aeabcde710edd80a4dfb81ebd2
Matt Kim <kimm5294@gmail.com>
2017-02-20 15:36:34 -0500

    add ord method and count

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -32,12 +32,13 @@
 # "The mystery of the missing first letter".delete("T")
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# "Elementary,    my   dear        Watson!".squeeze
+# "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+# "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
