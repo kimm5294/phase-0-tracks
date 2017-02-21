4be77db4d03c25f8a8cc51772df280bf7b3916f4
Matt Kim <kimm5294@gmail.com>
2017-02-20 23:03:10 -0500

    add part of code allowing update of prior entries and print hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -44,4 +44,48 @@ until pets == "y" || pets == "n"
     puts "Please enter a valid entry (y/n)"
   end
 end 
+
+puts "Would you like to make any changes to the information entered? Please type in the entry you would like to change: name, age, children, decor theme or pets. If you are done please enter done"
+update = gets.chomp 
+
+counter = 0
+while counter == 0
+  if update == "name"
+    puts "Please enter the new value you would like to enter for name:"
+    client_info[:client_name] = gets.chomp
+    counter += 1
+  elsif update == "age"
+    puts "Please enter the new value you would like to enter for age:"
+    client_info[:age] = gets.chomp.to_i
+    counter += 1
+  elsif update == "children"
+    puts "Please enter the new value you would like to enter for number of children:"
+    client_info[:number_children] = gets.chomp.to_i
+    counter += 1
+  elsif update == "decor theme"
+    puts "Please enter the new value you would like to enter for decor theme:"
+    client_info[:decor_theme] = gets.chomp
+    counter += 1
+  elsif update == "pets"
+    puts "Please enter the new value you would like to enter for pets (y/n)"
+    pets = ""
+    until pets == "y" || pets == "n" 
+      pets = gets.chomp
+      if pets == "y"
+        client_info[:has_pets] = true
+      elsif pets == "n"
+        client_info[:has_pets] = false
+      else 
+        puts "Please enter a valid entry (y/n)"
+      end
+    end
+    counter += 1
+  elsif update == "done"
+    puts "All set. Thank you!"
+    counter += 1
+  else 
+    puts "Please enter a valid entry"
+  end 
+end
+
 puts client_info 
