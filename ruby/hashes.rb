7f945200f7e461afdf14117a2616e1b55bef0a38
Matt Kim <kimm5294@gmail.com>
2017-02-20 19:22:04 -0500

    add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,13 @@
+=begin
+  -Create hash 
+  -Repeat steps for all information inputs
+    -Print statement prompting for information
+    -Convert input into proper data type to be stored in hash
+    -Have user input set to a variable and shovel value into hash key named after the variable  
+  -Print prompt asking if user wants to edit a key value pair
+  -Write code that takes input for the prompt and can be told which key if any the user wants to change
+    -If no key is to be changed then skip ahead to the end
+    -If user does want to change a key then prompt user for what they want to change the value to 
+    -Write code that changes the value for the key and overwrites the old one
+  -Print hash
+=end
