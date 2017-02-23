254a209641e8d3c037fbab4ad041508616bdf88a
Matt Kim <kimm5294@gmail.com>
2017-02-22 22:46:44 -0500

    finish adding info to nested data structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,13 +1,52 @@
 westeros {
  kings_landing: {
-    rulers: "The Lannisters"
+    rulers: "The Lannisters",
     noble_families: [
-    ]
- }
- the_vale: {
+      "Payne",
+      "Tyrell",
+      "Clegane"
+    ],
+    other_info: {
+      castle: "Red Keep",
+      religion: "the seven",
+      population: 1098712
+    }
+ },
 
- }
- winterfell: {
+ the_north: {
+  rulers: "House Stark",
+  noble_families: [
+      "Mormont",
+      "Glover",
+      "Bolton"
+  ],
+  other_info: {
+      castle: "Winterfell",
+      religion: "Old Gods",
+      population: 124313244
+  }
+ },
 
+ nights_watch: {
+  rulers: "Jon Snow",
+  noble_families: [
+      "Mormont",
+      "Stark"
+  ],
+  other_info: {
+    castle: "Castle Black",
+    religion: "Varied",
+    population: 100
+  }
  }
+ other_regions: [
+    "Braavos",
+    "Old Valyria",
+    "Qarth",
+    "Iron Isles"
+ ]
 }
+
+
+
+
