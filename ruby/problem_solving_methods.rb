bcd8bbcf9789f42f9f85ebfc9c21790c4e18ec5c
Matt Kim <kimm5294@gmail.com>
2017-02-22 21:51:05 -0500

    add pseudocode for fib sequence and create fib_sequence array

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,13 +1,15 @@
 =begin 
-Pseudocode
+Pseudocode for Simple Search
 -Find the length of the array
 -Find position in length of requested number
 -Print index value of given number
 =end
 
+#example array for simple search
 numbers = [42, 89, 23, 1, 7]
 
-def counter(array, number)
+#method for simple search
+def search(array, number)
   answer = nil
   array.length.times do |x|
     if array[x] == number 
@@ -20,5 +22,28 @@ def counter(array, number)
   end   
 end 
 
-puts counter(numbers, 7)
+#puts search(numbers, 7)
+
+=begin
+Pseudocode for Fibonacci Sequence 
+-create array of first two Fibonacci numbers [0, 1]
+-create a method that loops through and adds numbers to the array
+-add numbers to the array one at a time
+-add those numbers by adding sum of previous two numbers to array 
+-continue this loop until you get to desired index
+=end
+
+#starting array with 0 and 1 for Fibonacci sequence
+fib_sequence = [0, 1]
+
+def fib()
+  
+end 
+
+
+
+
+
+
+
 
