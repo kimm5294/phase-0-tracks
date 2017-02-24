b805fa8d102a39c15cd68fc5bb70a7400565c3a6
Matt Kim <kimm5294@gmail.com>
2017-02-23 23:52:22 -0500

    add method for vowel and consonant loops

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,7 +4,8 @@ Pseudocode
 -Store input into variable
 -Break up input into two separate strings and store each name in array as individual letters
 -Create two separate variables: one for vowels in order and the other for consonants in order
--Create two loops: one for vowels and one for consonants that change the letter to the next one
+-Create two separate methods: one for vowels and one for consonants that change the letter to the next one
+-Loop through the arrays for first and last name using the vowel and consonant method to change the letters
 -Join the letters back together to make the new name and print name as well as flipping first and last name
 =end 
 
@@ -12,11 +13,36 @@ Pseudocode
 
 #Method for splitting string
 def alias_generator(string)
-  split_name = string.split(" ")
+  split_name = string.downcase.split(" ")
   first_name = [split_name[1].chars]
   last_name = [split_name[0].chars]
+
+  vowels(first_name)
+
   puts first_name
   puts last_name
 end 
 
-#alias_generator("matt kim")
+
+
+def vowels(array)
+  vow = "aeiou"
+  vows = vow.chars
+  array.map do |x|
+    vows.map do |y|
+      if y == x 
+        x = vows[y+1]
+      end 
+    end 
+  end 
+end 
+
+
+def consonants(array)
+  con = "bcdfghjklmnpqrstvwxyz"
+  cons = con.chars
+
+end
+
+
+alias_generator("matt kim")
