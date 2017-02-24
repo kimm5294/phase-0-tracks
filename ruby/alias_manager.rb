a37c0fbd81e7819800aee8365cb9eeb4a1aa8d83
Matt Kim <kimm5294@gmail.com>
2017-02-23 22:28:24 -0500

    Add some more pseudocode

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,8 +2,9 @@
 Pseudocode
 -Prompt for input 
 -Store input into variable
--Break up input into two separate strings and store in array
+-Break up input into two separate strings and store each name in array as individual letters
 -Create two separate variables: one for vowels in order and the other for consonants in order
-
+-Create two loops: one for vowels and one for consonants that change the letter to the next one
+-Join the letters back together to make the new name and print name as well as flipping first and last name
 =end 
 
