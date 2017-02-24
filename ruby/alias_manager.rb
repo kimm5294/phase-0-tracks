ccd9825da1b0395706ca0eb647de3d6d7277bdf3
Matt Kim <kimm5294@gmail.com>
2017-02-23 22:22:34 -0500

    add some pseudocode

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,4 +1,9 @@
 =begin
 Pseudocode
-- 
+-Prompt for input 
+-Store input into variable
+-Break up input into two separate strings and store in array
+-Create two separate variables: one for vowels in order and the other for consonants in order
+
 =end 
+
