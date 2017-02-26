ff66c2c14b124f51701d4ee03c0111978d2198ff
Matt Kim <kimm5294@gmail.com>
2017-02-26 18:31:09 -0500

    add comment for puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+=begin 
+SPECIES ------------------
+Puppiosus Doggionium 
+
+CHARACTERISTICS ----------
+Cute: Yes
+Cuddly: Yes
+Color: White with black spots
+Knows Tricks: Yes
+Potty Trained: No
+
+BEHAVIOR -----------------
+Bark
+Sit
+Beg
+Shake
+Sleep
+=end 
