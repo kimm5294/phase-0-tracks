73862cdba67afb74fae027edd5be1d65d35860f5
Matt Kim <kimm5294@gmail.com>
2017-02-27 18:56:30 -0500

    add santa arr, arrays with genders and ethnicities and add 10 santas with random gender and ehtnicity

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -17,3 +17,12 @@ class Santa
   end 
 
 end 
+
+santas = []
+
+example_genders = ["male", "female", "agender", "bigender", "gender fluid", "N/A"]
+example_ethnicities = ["black", "asian", "latino", "native american", "pacific islander", "white", "other", "N/A"]
+
+#add 10 santas to santas array using rand(int) as a random number generator to choose a random gender/ethnicity
+10.times {santas << Santa.new(example_genders[rand(6)], example_ethnicities[rand(8)])}
+
