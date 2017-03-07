e916e949867179252455994b583e31bcfedbfd45
Matt Kim <kimm5294@gmail.com>
2017-03-06 23:13:23 -0500

    add pseudocode to new file

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+//Pseudocode for reversing a string
+
+//Put string in as input for a newly defined function
+//Loop through string and return the last letter of the string first and then the second to last and so on
+//Add each letter together 
+//Print to console
+
