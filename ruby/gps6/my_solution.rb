ba38da1e2b88f66d96da4cfa9e98270a10ccfd1b
Matt Kim <kimm5294@gmail.com>
2017-03-06 22:08:46 -0500

    refactor methods in class

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -20,27 +20,30 @@ class VirusPredictor
 
   #This method will call both other methods in the class which will print statements regarding predicted deaths and speed of spread
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
   #This method predicts number of deaths based on population density and population then returns a statement for each state and its 
   #predicted deaths 
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
+
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      x = 0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      x = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      x = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      x = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      x = 0.05
     end
+
+    number_of_deaths = (@population * x).floor
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
@@ -48,21 +51,20 @@ class VirusPredictor
 
   #This method uses population density to predict the speed of the spread of the virus in months and then prints a statment
   #saying how fast it will spread across the state
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      speed = 0.5
     elsif @population_density >= 150
-      speed += 1
+      speed = 1
     elsif @population_density >= 100
-      speed += 1.5
+      speed = 1.5
     elsif @population_density >= 50
-      speed += 2
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
@@ -76,9 +78,11 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
-
+=begin
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
+
+alabama.speed_of_spread
 
 jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
 jersey.virus_effects
@@ -88,7 +92,13 @@ california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
+=end 
 
+STATE_DATA.each do |key, value|
+  x = VirusPredictor.new(key, STATE_DATA[key][:population_density], STATE_DATA[key][:population])
+  x.virus_effects 
+end 
 
 #=======================================================================
 # Reflection Section
+
