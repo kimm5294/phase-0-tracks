570429e0983c28ddb9b631f31b166f0493dede06
Matt Kim <kimm5294@gmail.com>
2017-03-06 23:39:09 -0500

    add comments and driver code to test method

--- js/explore.js
+++ js/explore.js
@@ -5,3 +5,22 @@
 //Add each letter together 
 //Print to console
 
+//Define the function reverse to reverse the string that is the input
+function reverse(string) {
+  var reversedString = ""
+  for (var i = 1; i <= string.length; i++) {
+    reversedString += string[string.length-i]
+  }
+  return reversedString
+}
+
+//Define a variable to equal the reversed string 
+var x = reverse("hello")
+
+//Print the reversed string if statements are true
+
+if (1 == 1 && 2 ==2) {
+  console.log(x)
+} else {
+  console.log("Too bad...")
+}
