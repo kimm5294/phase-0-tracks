1820cb944968960042dcfbfa45d040ea324440b0
Matt Kim <kimm5294@gmail.com>
2017-03-08 14:25:27 -0500

    add pseudocode for longest word method

---
+++ js/algos.js
@@ -0,0 +1,12 @@
+//Method that returns the longest word/phrase of an array
+//Input: an array of words/phrases
+//Method:
+//Define function with array of words/phrases as input
+//Define a variable that will store the longest word/phrase
+//Loop through words in the array
+//IF current word is longer than the word stored in the variable, set 
+//variable equal to the current word
+//ELSE do nothing 
+//Return the variable that stores the longest word
+//Output: Longest word/phrase of the array
+
