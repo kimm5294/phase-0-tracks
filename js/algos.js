c7eead6f623bd70164aa34ee69dd69cef4fe195f
Matt Kim <kimm5294@gmail.com>
2017-03-08 14:34:46 -0500

    create function to find longest word in array and add driver code to test it

--- js/algos.js
+++ js/algos.js
@@ -10,3 +10,23 @@
 //Return the variable that stores the longest word
 //Output: Longest word/phrase of the array
 
+function findLongWord(arrayOfWords) {
+  var longestWord = "a";
+  for (var i = 0; i < arrayOfWords.length; i++) {
+    if (arrayOfWords[i].length > longestWord.length) {
+      longestWord = arrayOfWords[i];
+    }
+  }
+
+
+  return longestWord;
+}
+
+//Driver Code to test longest word finding method
+var array1 = ["hello", "hi", "what is good"];
+var array2 = ["pink", "red", "blue", "yellow"];
+var x = findLongWord(array1);
+var y = findLongWord(array2);
+console.log(x);
+console.log(y);
+
