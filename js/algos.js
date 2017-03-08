c4a7827e9e6ea9c2de2c05873e713c7831b1aa51
Matt Kim <kimm5294@gmail.com>
2017-03-08 14:51:37 -0500

    add driver code to test second method about key-value pairs

--- js/algos.js
+++ js/algos.js
@@ -26,14 +26,22 @@ function findLongWord(arrayOfWords) {
 //Input: two objects with key-value pairs
 //Method:
 //Define a function that takes two objects as input
+//Set a variable for the boolean equal to false
 //Do two levels of loops to loop through each objects keys and compare
-//IF objects have the same key then return true if the values of those keys are also the same
-//ELSE return false
+//IF objects have the same key then boolean = true if the values of those keys are also the same
+//ELSE do nothing
 //Output: boolean
 
 function matchKeyValue(object1, object2) {
-
-  
+  var boolean = false;
+  for (var key1 in object1) {
+    for (var key2 in object2) {
+      if (key1 == key2 && object1[key1] == object2[key2]) {
+        boolean = true;
+      }
+    }
+  }
+  return boolean;
 }
 
 
@@ -42,11 +50,19 @@ function matchKeyValue(object1, object2) {
 //DRIVER CODE
 
 //Driver Code to test longest word finding method
-var array1 = ["hello", "hi", "what is good"];
-var array2 = ["pink", "red", "blue", "yellow"];
-var x = findLongWord(array1);
-var y = findLongWord(array2);
-console.log(x);
-console.log(y);
+//var array1 = ["hello", "hi", "what is good"];
+//var array2 = ["pink", "red", "blue", "yellow"];
+//var x = findLongWord(array1);
+//var y = findLongWord(array2);
+//console.log(x);
+//console.log(y);
+
+
+//Driver Code to test second method that returns boolean for matching key-value pair
+var obj1 = {name: "MK", age: 3, boo: true};
+var obj2 = {name: "M", age: 5, boo: false};
+var z = matchKeyValue(obj1, obj2);
+console.log(z);
+
 
 
