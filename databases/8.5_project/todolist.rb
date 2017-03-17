4918d6b2f6ac37247340635dfad158c2c52ef648
Matt Kim <kimm5294@gmail.com>
2017-03-16 23:32:56 -0400

    add some comments explaining what the program is

--- databases/8.5_project/todolist.rb
+++ databases/8.5_project/todolist.rb
@@ -1,2 +1,6 @@
-# PSEUDOCODE
+#This program will create and store a to do list
+#Functions will include showing the list, checking things off, showing completed items, 
+#Deleting items from list or editing them and putting a due date on them
 
+# PSEUDOCODE
+#
