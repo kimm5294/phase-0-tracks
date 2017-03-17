8c28ea579873f85ff1d89cd0451ddee9d56cfb07
Matt Kim <kimm5294@gmail.com>
2017-03-17 00:30:30 -0400

    create database, create table in database

--- databases/8.5_project/todolist.rb
+++ databases/8.5_project/todolist.rb
@@ -6,12 +6,31 @@
 #Require the needed gems
 require "sqlite3"
 
-# PSEUDOCODE
-#Need:
+#Create the sqlite3 database
+database = SQLite3::Database.new("todolist.db")
+database.results_as_hash = true 
+
+#Create the table if it doesn't exist already 
+create_table_cmd = <<-yoyoyo
+  CREATE TABLE IF NOT EXISTS todolist (
+  id INTEGER PRIMARY KEY, 
+  item VARCHAR(255),
+  date DATE
+  )
+yoyoyo
+database.execute(create_table_cmd)
+
+#Needed methods:
   # A method that will display to do list
+    #No input
+    #Print out the to do list item and due date
+
   # A method that will edit to do list items
+    #Input: 
+    #
+
   # A method that will add to do list items
   # A method that will mark items as done
-  # A method that will make a new list
   # A method that will delete to do list items 
-
+  # A method that will make a new list
+    #This method should create a new table and allow it to be named
