a3d2690410c0aa8ae1fa9307afd7724047516358
Matt Kim <kimm5294@gmail.com>
2017-03-16 23:38:01 -0400

    require sqlite3 and add some pseudcode for methods

--- databases/8.5_project/todolist.rb
+++ databases/8.5_project/todolist.rb
@@ -1,6 +1,17 @@
 #This program will create and store a to do list
-#Functions will include showing the list, checking things off, showing completed items, 
-#Deleting items from list or editing them and putting a due date on them
+#Functions will include showing the list, making a new list, checking things off, 
+#showing completed items, deleting items from list or editing them 
+#and putting a due date on them
+
+#Require the needed gems
+require "sqlite3"
 
 # PSEUDOCODE
-#
+#Need:
+  # A method that will display to do list
+  # A method that will edit to do list items
+  # A method that will add to do list items
+  # A method that will mark items as done
+  # A method that will make a new list
+  # A method that will delete to do list items 
+
