dcaff72037352996d5703f92aedc00ada9800ffb
Matt Kim <kimm5294@gmail.com>
2017-03-22 00:17:40 -0400

    add code to toggle fade of header and change html of main header

--- web_dev/jquery/jquery.js
+++ web_dev/jquery/jquery.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
 
+  //The first parentheses tell which tag to affect, the .html changes the html in element
+  $("#title").html("New News");
+
+  //Make items toggle out of sight with a click of the button
+  $("#fade").click(function() {
+    $("h1").fadeToggle();
+  });
+
 }); 
