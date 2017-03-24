b25d94e647933f2832dc3ca5a81647b0c6cd74b1
Matt Kim <kimm5294@gmail.com>
2017-03-24 12:58:59 -0400

    create route to new template that displays name and campus

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -24,4 +24,13 @@ post '/students' do
   redirect '/'
 end
 
+
 # add static resources
+
+#Module 9.5
+#show student names and campus
+get '/student_campus' do 
+  @students = db.execute("SELECT * FROM students")
+  erb :template
+end 
+
